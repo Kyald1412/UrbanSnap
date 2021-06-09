656abdb989faa6afbf50a1644e9ba59a6a877674
Nuuru Alhusna Shufiya Putri <nuurualhusnaputri@gmail.com>
2021-06-09 09:33:23 +0700

    assets

--- UrbanSnap/Helpers/Extensions/UIImage+Extension.swift
+++ UrbanSnap/Helpers/Extensions/UIImage+Extension.swift
@@ -12,6 +12,14 @@ import UIKit
 
 extension UIImage {
 
+    static let badge = UIImage.image(named: "Badge")
+    static let onboardingLearn = UIImage.image(named: "Onboarding-Learn")
+    static let onboardingPractice = UIImage.image(named: "Onboarding-Practice")
+    static let onboardingReflect = UIImage.image(named: "Onboarding-Reflect")
+    static let padlock = UIImage.image(named: "Padlock")
+    static let ilustrasiLevel1 = UIImage.image(named: "ilustrasi level 1")
+    static let ilustrasiLevel2 = UIImage.image(named: "ilustrasi level 2")
+    static let ilustrasiLevel3 = UIImage.image(named: "ilustrasi level 3")
     static let placeholder = UIImage.image(named: "placeholder")
 
     private static func image(named: String) -> UIImage {
