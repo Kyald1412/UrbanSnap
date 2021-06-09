fb3699a33db2b4488042e6f5d41a2f703a930ae0
AtikahBZqAulia <atikahap.yes@gmail.com>
2021-06-09 14:41:04 +0700

    Add struct and dummy data

--- UrbanSnap/Presentation/Challenge/Challenge List/ChallengeListScene.swift
+++ UrbanSnap/Presentation/Challenge/Challenge List/ChallengeListScene.swift
@@ -8,22 +8,31 @@
 import UIKit
 
 class ChallengeListScene: UIViewController {
-
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return challengeList.count
+//    }
+    
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let item = challengeList[indexPath.row]
+//        let cell = challengeTableView.dequeueReusableCell(withIdentifier: "challengeItem", for: indexPath) as! ChallengeListTableViewCell
+//        cell.update(with: item)
+//        print("CELL \(cell)")
+//        return cell
+//    }
+    
+    @IBOutlet weak var challengeTableView: UITableView!
+    
+    private let challengeList = [ Challenge(level: "Level 1", desc: "Belajar fotografi bersama kami", image: "yes"), Challenge(level: "Level 2", desc: "Belajar fotografi bersama kami", image: "yes"), Challenge(level: "Level 3", desc: "Belajar fotografi bersama kami", image: "yes")]
     override func viewDidLoad() {
+        
+//        challengeTableView.register(UITableViewCell.self, forCellReuseIdentifier: "challengeItem")
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    struct Challenge{
+        let level : String
+        let desc: String
+        let image: String
     }
-    */
 
 }
