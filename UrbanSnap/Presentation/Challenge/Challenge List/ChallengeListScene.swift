7ba8c77292c0583618165a67b5d2a949b01ec961
AtikahBZqAulia <atikahap.yes@gmail.com>
2021-06-10 10:12:34 +0700

    Set rounded challenge list table view cell

--- UrbanSnap/Presentation/Challenge/Challenge List/ChallengeListScene.swift
+++ UrbanSnap/Presentation/Challenge/Challenge List/ChallengeListScene.swift
@@ -15,14 +15,16 @@ class ChallengeListScene: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let item = challengeList[indexPath.row]
         let cell = challengeTableView.dequeueReusableCell(withIdentifier: "challengeItem", for: indexPath) as! ChallengeListTableViewCell
-//        cell.update(with: item)
+        cell.setLevelList(with: item)
 //        print("CELL \(cell)")
+        cell.layer.cornerRadius = 10
+        cell.layer.masksToBounds = true
         return cell
     }
     
     @IBOutlet weak var challengeTableView: UITableView!
     
-    private let challengeList = [ Challenge(level: "Level 1", desc: "Belajar fotografi bersama kami", image: "yes"), Challenge(level: "Level 2", desc: "Belajar fotografi bersama kami", image: "yes"), Challenge(level: "Level 3", desc: "Belajar fotografi bersama kami", image: "yes")]
+    private let challengeList = [ Challenge(level: "Level 1", desc: "Learn to shoot still urban objects with two layers", image: "yes"), Challenge(level: "Level 2", desc: "Learn to shoot still urban objects with three layers", image: "yes"), Challenge(level: "Level 3", desc: "Learn to shoot moving urban objects with two layers", image: "yes")]
     override func viewDidLoad() {
         
 //        challengeTableView.register(UITableViewCell.self, forCellReuseIdentifier: "challengeItem")
