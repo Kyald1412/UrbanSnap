9bacfac2ec81a76220b20958df841e72c1deb626
AtikahBZqAulia <atikahap.yes@gmail.com>
2021-06-11 08:38:08 +0700

    Add view of Evaluation page

---
+++ UrbanSnap/Presentation/Reflection/Reflection List/GalleryListTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  GalleryListTableViewCell.swift
+//  UrbanSnap
+//
+//  Created by Atikah Aulia on 10/06/21.
+//
+
+import UIKit
+
+class GalleryListTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var galleryCollectionView : UICollectionView!
+
+}
