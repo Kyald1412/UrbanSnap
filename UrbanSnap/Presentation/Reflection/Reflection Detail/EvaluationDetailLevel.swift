2675d6db8c2f95490ca62ba23736a137b9a1e2d5
Wilson Adrilia <wilsonadrilia@gmail.com>
2021-06-14 19:38:43 +0700

    EvaluationDetail View

---
+++ UrbanSnap/Presentation/Reflection/Reflection Detail/EvaluationDetailLevel.swift
@@ -0,0 +1,38 @@
+//
+//  EvaluationDetailLevel.swift
+//  UrbanSnap
+//
+//  Created by Wilson Adrilia on 14/06/2021.
+//
+
+import UIKit
+
+class EvaluationDetailLevel: UIViewController {
+
+    @IBOutlet weak var descLabel: UILabel!
+    @IBOutlet weak var descTextView: UITextView!
+    @IBOutlet weak var emptyLabel1: UILabel!
+    @IBOutlet weak var emptyLabel2: UILabel!
+    @IBOutlet weak var evaluationImage: UIImageView!
+    @IBOutlet weak var evaluationButton: UIButton!
+    @IBOutlet weak var descView: UIView!
+    @IBOutlet weak var emptyView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
