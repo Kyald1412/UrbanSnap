c52129ccd6af45e164d91b91e6e8411e9e59cb41
Dhiky Aldwiansyah <dhikyaldwiansyah@gmail.com>
2021-07-22 10:45:06 +0700

    update

--- UrbanSnap/Presentation/ViewController.swift
+++ UrbanSnap/Presentation/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //sadjsahdgshjadsadasdasghj
+        // sadsadasddsajkdhaskjdhasdhkjshdjh
         // Do any additional setup after loading the view.
     }
 
